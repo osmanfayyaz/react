cf87b205c84bff7f9ce7c3e0d4ba8906c37d5d79
osmanfayyaz <osmanfayyaz32@gmail.com>
2021-04-22 15:58:50 +0500

    maping

--- src/App.js
+++ src/App.js
@@ -6,50 +6,49 @@ import UserInput from './UserInput/UserInput';
 import UserOutput from './UserOutput/UserOutput';
 
 class App extends Component {
-// state={
-// username:'munib'
-// }
-   
-// render(){
-//   return(
-//     <div className="App">
-//       <ol>
-//       <li>sdsdadasdsa</li>
-//       <li>sdsdadasdsa</li>
-//       <li>sdsdadasdsa</li>
-//       <UserInput/>
-//       <UserOutput userName={this.state.username}/>
-//       <UserOutput userName='Max'/>
-//       </ol>
-//     </div>
-//   );
-// }
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      persons:[
+        {name:'Osman',age:27,status:'single'},
+        {name:'Dawood',age:23,status:'single'},
+        {name:'Munib',age:25,status:'Married'}
+        
+      ],
+      otherState:'some other value',
+      showPersons: false
+     }
+    }
 
 
   
-  state={
-    person:[
-      {name:'Osman',age:27,status:'single'},
-      {name:'Dawood',age:23,status:'single'},
-      {name:'Munib',age:25,status:'Married'}
+  // state={
+  //   person:[
+  //     {name:'Osman',age:27,status:'single'},
+  //     {name:'Dawood',age:23,status:'single'},
+  //     {name:'Munib',age:25,status:'Married'}
       
-    ],
-    otherState:'some other value',
-    showPersons: false
-  }
-  personHandler= (newName)=>{
-    // console.log("kicked");
-    this.setState({
-      person:[
-        {name:newName,age:27,status:'single'},
-        {name:'Ali',age:23,status:'single'},
-        {name:'Rizwan',age:25,status:'Married'}
+  //   ],
+  //   otherState:'some other value',
+  //   showPersons: false
+  // }
+  // personHandler= (newName)=>{
+  //   // console.log("kicked");
+  //   this.setState({
+  //     person:[
+  //       {name:newName,age:27,status:'single'},
+  //       {name:'Ali',age:23,status:'single'},
+  //       {name:'Rizwan',age:25,status:'Married'}
         
-      ]
-    })
-  }
+  //     ]
+  //   })
+  // }
 
+  deleteNameHandler = (personIndex)=>{
+    const persons = this.state.persons;
+    persons.splice(personIndex,1);
+    this.setState({persons:persons})
+  }
 
   nameChangedHandler=(event)=>{
     this.setState({
@@ -62,9 +61,8 @@ class App extends Component {
   }
 
   togglepersonHandler =()=>{
-      const doesShow = this.state.showPersons;
-      this.setState({showPersons: !doesShow});
-  }
+    this.setState({showPersons: !this.state.showPersons});
+}
 
   render() {
 
@@ -75,13 +73,7 @@ class App extends Component {
       padding:'9px',
       cursor:'pointer'
     }
-
-    let persons = null;
-    if(this.state.showPersons){
-      persons=(
- 
-        <div>
-        <Person
+        {/* <Person
          name={this.state.person[0].name} 
          age={this.state.person[0].age} 
          status={this.state.person[0].status}/>   
@@ -95,22 +87,32 @@ class App extends Component {
          age={this.state.person[2].age}       
          status={this.state.person[2].status}
          click={this.personHandler.bind(this,'abeshak')}
-         >My Hobbies:username</Person>   
-        </div>
-      );
-    }
-    return ( 
+         >My Hobbies:username</Person>    */}
+    //     </div>
+    //   );
+    // }
+    return (
+         
+        
       <div className="App">
          <h1>Welcome To Ecare!!!</h1> 
          <button style={style}
          onClick={this.togglepersonHandler}>Switch</button>
-         {persons}         
+                
+                <div>
+           {this.state.showPersons === true && this.state.persons.map((p,index)=>(
+            <Person
+            click={()=>this.deleteNameHandler(index)}
+            name={p.name}
+            age={p.age}
+            status={p.status}
+            />
+          ))}
+          </div>
       </div>
-      
-    );
-    // return React.createElement('div',{className:'App'}, React.createElement('h1',null,'Welcome To Ecare!!!'));
-  }
-
+     
+    )
+}
 }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,15 +4,11 @@ const person= (props) =>{
     return (
 
         <div>
-
         <p onClick={props.click}>Hye I am <b>{props.name}</b> my age is <b>{props.age}</b> and i am <b>{props.status}</b></p>
         {/* <p>Hye I am <b>{props.name}</b> my age is <b>{props.age}</b> and i am <b>{props.status}</b></p>,
         <p>Hye I am <b>{props.name}</b> my age is <b>{props.age}</b> and i am <b>{props.status}</b></p>  */}
         <input type='text' onChange={props.changed} value={props.name}/>
         </div>
-       
-    
-    
     );
 }
 export default person;
